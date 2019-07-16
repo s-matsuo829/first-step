f29830505f34d45bb6da2f4455d1af1b7399ac5b
s-matsu829 <fakefur829@gmail.com>
2019-07-16 09:54:36 +0900

    create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_time
 
   def index
+    @message = Message.new
+    @messages = Message.includes(:user)
   end
-  
+
+  def create
+    @message = Message.create(image: message_params[:image], content: message_params[:content], user_id: current_user.id)
+    if @message.save
+      redirect_to root_path
+    else
+      @messages = Message.includes(:user)
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_time
+    wd = ["Sun", "Mon", "tue", "Wed", "Thu", "Fri", "Sat"]
+    time = Time.now
+    @time = wd[time.wday]
+  end
 end
